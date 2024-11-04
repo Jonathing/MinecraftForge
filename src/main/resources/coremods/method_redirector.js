5bc04976aac98ed5c1d09f831db8aa181f68a19d
Jonathing <me@jonathing.me>
2024-11-04 12:17:47 -0500

    Print an error if no redirections were applied to a target class

--- src/main/resources/coremods/method_redirector.js
+++ src/main/resources/coremods/method_redirector.js
@@ -9,9 +9,12 @@ function initReplacements() {
     ASMAPI.log('DEBUG', 'Gathering Forge method redirector replacements');
     replacements.push({
         // finalizeSpawn redirection to ForgeEventFactory.onFinalizeSpawn
+        // TODO BROKEN!!! Fix by using the newer descriptor in the below comment.
         'type': ASMAPI.MethodType.VIRTUAL,
         'name': 'finalizeSpawn',
         'desc': '(Lnet/minecraft/world/level/ServerLevelAccessor;Lnet/minecraft/world/DifficultyInstance;Lnet/minecraft/world/entity/MobSpawnType;Lnet/minecraft/world/entity/SpawnGroupData;)Lnet/minecraft/world/entity/SpawnGroupData;',
+               // TODO Fix the descriptor to match the actual method signature. Right now, this CoreMod doesn't apply at all.
+               //(Lnet/minecraft/world/level/ServerLevelAccessor;Lnet/minecraft/world/DifficultyInstance;Lnet/minecraft/world/entity/EntitySpawnReason;Lnet/minecraft/world/entity/SpawnGroupData;)Lnet/minecraft/world/entity/SpawnGroupData;
         'targets': ASMAPI.loadData('coremods/finalize_spawn_targets.json'),
         'factory': function(insn) {
             return ASMAPI.buildMethodCall(
@@ -49,6 +52,7 @@ function getTargets(classes) {
 }
 
 function applyMethodRedirects(clazz) {
+    let applied = 0;
     for (let method of clazz.methods) {
         for (let insn of method.instructions) {
             const replacement = search(clazz.name, insn, replacements);
@@ -56,8 +60,13 @@ function applyMethodRedirects(clazz) {
                 const redirection = replacement.factory(insn);
                 ASMAPI.log('DEBUG', 'Redirecting method call {}{} to {}{} inside of {}.{}', insn.name, insn.desc, redirection.name, redirection.desc, clazz.name, method.name);
                 ASMAPI.insertInsn(method, insn, redirection, ASMAPI.InsertMode.REMOVE_ORIGINAL);
+                applied++;
             }
         }
+    }
+
+    if (applied === 0) {
+        ASMAPI.log('ERROR', 'No method redirections applied to class {}! This is likely due to a type change in a Minecraft update. Please check the implementation!', clazz.name)
     }
 
     return clazz;
