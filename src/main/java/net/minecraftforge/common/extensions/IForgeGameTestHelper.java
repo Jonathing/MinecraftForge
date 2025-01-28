95dac8c46c1f6b1c863b8e3194265af706e53a9f
Jonathing <me@jonathing.me>
2025-01-28 14:40:47 -0500

    IntFlag enhancements in IForgeGameTestHelper

--- src/main/java/net/minecraftforge/common/extensions/IForgeGameTestHelper.java
+++ src/main/java/net/minecraftforge/common/extensions/IForgeGameTestHelper.java
@@ -14,6 +14,7 @@ import java.util.function.Supplier;
 import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.ChatFormatting;
+import net.minecraft.Util;
 import net.minecraft.network.chat.MutableComponent;
 import org.jetbrains.annotations.Nullable;
 
@@ -193,6 +194,14 @@ public interface IForgeGameTestHelper {
         return new IntFlag(name);
     }
 
+    default IntFlag intFlag(String name, int value) {
+        return this.intFlag(name, (long) value);
+    }
+
+    default IntFlag intFlag(String name, long value) {
+        return Util.make(new IntFlag(name), flag -> flag.set(value));
+    }
+
     default BoolFlag boolFlag(String name) {
         return new BoolFlag(name);
     }
@@ -269,6 +278,32 @@ public interface IForgeGameTestHelper {
 
         public void set(long value) {
             super.set(value);
+        }
+
+        public void increment() {
+            this.decrement(1L);
+        }
+
+        public void increment(int amount) {
+            this.decrement((long) amount);
+        }
+
+        public void increment(long amount) {
+            if (this.value != null)
+                this.set(this.value + amount);
+        }
+
+        public void decrement() {
+            this.decrement(1L);
+        }
+
+        public void decrement(int amount) {
+            this.decrement((long) amount);
+        }
+
+        public void decrement(long amount) {
+            if (this.value != null)
+                this.set(this.value - amount);
         }
 
         public byte getByte() {
