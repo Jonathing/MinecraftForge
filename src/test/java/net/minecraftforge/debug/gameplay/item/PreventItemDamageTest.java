bde422d80bf99032a063fc40776e2d1a0dd95da4
Jonathing <me@jonathing.me>
2025-02-01 13:19:42 -0500

    Finish game test, add more use cases and expand GameTestHelper

--- src/test/java/net/minecraftforge/debug/gameplay/item/PreventItemDamageTest.java
+++ src/test/java/net/minecraftforge/debug/gameplay/item/PreventItemDamageTest.java
@@ -13,13 +13,12 @@ import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
 import net.minecraft.world.item.ShieldItem;
 import net.minecraft.world.level.GameType;
 import net.minecraftforge.event.entity.living.LivingEntityUseItemEvent;
+import net.minecraftforge.event.entity.player.PlayerDestroyItemEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.gametest.GameTestHolder;
@@ -30,7 +29,6 @@ import net.minecraftforge.test.BaseTestMod;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.function.Consumer;
-import java.util.function.IntUnaryOperator;
 
 @Mod(PreventItemDamageTest.MOD_ID)
 @GameTestHolder("forge." + PreventItemDamageTest.MOD_ID)
@@ -38,17 +36,11 @@ public class PreventItemDamageTest extends BaseTestMod {
     static final String MOD_ID = "prevent_item_damage";
 
     private static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MOD_ID);
-    private static final RegistryObject<Item> FAKE_SHIELD = ITEMS.register("fake_shield", () -> new ShieldItem(new Item.Properties().setId(ITEMS.key("fake_shield")).durability(10)) {
-        @Override
-        public int damageItem(ItemStack stack, int damage, ServerLevel level, @Nullable ServerPlayer player, Consumer<Item> onBroken) {
-            onBroken.accept(this);
-            return 0;
-        }
-    });
+    private static final RegistryObject<FakeShieldItem> FAKE_SHIELD = ITEMS.register("fake_shield", FakeShieldItem::new);
 
     public PreventItemDamageTest(FMLJavaModLoadingContext context) {
         super(context);
-        this.testItem(lookup -> new ItemStack(FAKE_SHIELD.get()));
+        this.testItem(lookup -> FAKE_SHIELD.get().getDefaultInstance());
     }
 
     @GameTest(template = "forge:empty3x3x3")
@@ -57,16 +49,34 @@ public class PreventItemDamageTest extends BaseTestMod {
 
         // setup player
         var player = helper.makeMockPlayer(GameType.SURVIVAL);
+
+        // setup shield
+        var shield = FAKE_SHIELD.get().getDefaultInstance();
+
+        // setup events
+        var firedLivingEntityUseItem = helper.boolFlag("fired LivingEntityUseItemEvent");
+        var firedPlayerDestroyItem = helper.boolFlag("fired PlayerDestroyItemEvent");
         helper.<LivingEntityUseItemEvent.Start>addEventListener(event -> {
+            if (event.getEntity() != player) return;
+
+            if (event.getItem() != shield)
+                helper.fail("Player is using an item, but it's not the fake shield! Check the game test impl.");
+
             // Artificially pass 5 seconds from start of the shield
             // This is because the first 5 ticks, the player is still vulnerable
-            if (event.getEntity() == player) {
-                event.setDuration(event.getDuration() - 100);
-            }
+            firedLivingEntityUseItem.set(true);
+            event.setDuration(event.getDuration() - 100);
+        });
+        helper.<PlayerDestroyItemEvent>addEventListener(event -> {
+            if (event.getEntity() != player) return;
+
+            if (event.getOriginal() != shield)
+                helper.fail("Player destroyed an item, but it's not the fake shield! Check the game test impl.");
+
+            firedPlayerDestroyItem.set(true);
         });
 
         // start using shield
-        var shield = FAKE_SHIELD.get().getDefaultInstance();
         player.setItemInHand(InteractionHand.MAIN_HAND, shield);
         player.startUsingItem(InteractionHand.MAIN_HAND);
         int initialDamage = shield.getDamageValue();
@@ -78,8 +88,11 @@ public class PreventItemDamageTest extends BaseTestMod {
         // hit the player
         player.hurtServer(helper.getLevel(), enemy.damageSources().mobAttack(enemy), 5.0F);
 
-        // shield on cooldown?
-        helper.assertTrue(initialDamage == shield.getDamageValue(), "Fake shield took damage! Expected: " + initialDamage + ", Actual: " + shield.getDamageValue());
+        // ok, run the tests
+        firedLivingEntityUseItem.assertEquals(true);
+        firedPlayerDestroyItem.assertEquals(true);
+        helper.assertValueEqual(initialDamage + 1, shield.getDamageValue(), "shield damage value", "Fake shield did not take precisely 1 damage! Check IForgeItem#damageItem.");
+        helper.assertValueEqual(player.getItemInHand(InteractionHand.MAIN_HAND), shield, "player shield", "Fake shield was removed from player's hand! Check Player#hurtCurrentlyUsedShield.");
         helper.succeed();
     }
 
