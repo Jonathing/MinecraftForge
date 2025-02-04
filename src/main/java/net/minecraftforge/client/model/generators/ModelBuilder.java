5e5121fcf4d9ba544257d892c1ca70ed490dceeb
Jonathing <me@jonathing.me>
2025-02-03 20:59:01 -0500

    Add fast render type option to providers

--- src/main/java/net/minecraftforge/client/model/generators/ModelBuilder.java
+++ src/main/java/net/minecraftforge/client/model/generators/ModelBuilder.java
@@ -171,6 +171,7 @@ public class ModelBuilder<T extends ModelBuilder<T>> extends ModelFile {
     public T renderType(ResourceLocation renderType) {
         Preconditions.checkNotNull(renderType, "Render type must not be null");
         this.renderType = renderType.toString();
+        this.renderTypeFast = null;
         return self();
     }
 
