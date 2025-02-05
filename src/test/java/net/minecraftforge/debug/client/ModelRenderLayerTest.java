14681d0965bb5909b9626438fcd088531d327dc6
Jonathing <me@jonathing.me>
2025-02-04 21:27:36 -0500

    Initial test mod

--- src/test/java/net/minecraftforge/debug/client/ModelRenderLayerTest.java
+++ src/test/java/net/minecraftforge/debug/client/ModelRenderLayerTest.java
@@ -5,6 +5,7 @@
 
 package net.minecraftforge.debug.client;
 
+import net.minecraft.ChatFormatting;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.data.models.BlockModelGenerators;
 import net.minecraft.client.data.models.ItemModelGenerators;
@@ -26,6 +27,7 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.LeavesBlock;
 import net.minecraft.world.level.block.SaplingBlock;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.grower.TreeGrower;
@@ -72,8 +74,28 @@ public class ModelRenderLayerTest extends BaseTestMod {
         )
     );
 
+    private static final String OLD_LEAVES_NAME = "old_leaves";
+    private static final BlockBehaviour.StatePredicate FALSE = (state, level, pos) -> false;
+    public static final RegistryObject<LeavesBlock> OLD_LEAVES = BLOCKS.register(OLD_LEAVES_NAME,
+        () -> new LeavesBlock(
+            BlockBehaviour.Properties.of()
+                .setId(BLOCKS.key(OLD_LEAVES_NAME))
+                .mapColor(MapColor.PLANT)
+                .strength(0.2F)
+                .randomTicks()
+                .sound(SoundType.GRASS)
+                .noOcclusion()
+                .isSuffocating(FALSE)
+                .isViewBlocking(FALSE)
+                .ignitedByLava()
+                .pushReaction(PushReaction.DESTROY)
+                .isRedstoneConductor(FALSE)
+        )
+    );
+
     private static final DeferredRegister<Item> ITEMS = DeferredRegister.create(Registries.ITEM, MODID);
     public static final RegistryObject<Item> ITEM = ITEMS.register(BLOCK_NAME, () -> new BlockItem(BLOCK.get(), new Item.Properties().setId(ITEMS.key(BLOCK_NAME))));
+    public static final RegistryObject<BlockItem> OLD_LEAVES_ITEM = ITEMS.register(OLD_LEAVES_NAME, () -> new BlockItem(OLD_LEAVES.get(), new Item.Properties().setId(ITEMS.key(OLD_LEAVES_NAME))));
 
     private static ResourceLocation rl(String path) {
         return ResourceLocation.fromNamespaceAndPath(MODID, path);
@@ -104,8 +126,25 @@ public class ModelRenderLayerTest extends BaseTestMod {
         var state = BLOCK.get().defaultBlockState();
         var random = helper.getLevel().random;
         var layer = model.getRenderTypes(state, random, ModelData.EMPTY);
-        if (!layer.contains(RenderType.cutout()))
-            helper.fail("Block model does not have cutout layer");
+        helper.assertTrue(layer.contains(Minecraft.useFancyGraphics() ? RenderType.cutout() : RenderType.solid()), "Block model does not contain the current render type for the current graphics settings. Setting: " + Minecraft.useFancyGraphics() + " Expected: " + (Minecraft.useFancyGraphics() ? RenderType.cutout() : RenderType.solid()));
+
+        helper.succeed();
+    }
+
+    @GameTest(template = "forge:empty3x3x3")
+    public static void old_leaves_render_type(GameTestHelper helper) {
+        var manager = Minecraft.getInstance().getModelManager();
+
+        var key = new ModelResourceLocation(rl(OLD_LEAVES_NAME), "");
+        var model = manager.getModel(key);
+
+        if (model == manager.getMissingModel())
+            helper.fail("Failed to retreive " + key + " block model");
+
+        var state = OLD_LEAVES.get().defaultBlockState();
+        var random = helper.getLevel().random;
+        var layer = model.getRenderTypes(state, random, ModelData.EMPTY);
+        helper.assertTrue(layer.contains(Minecraft.useFancyGraphics() ? RenderType.cutoutMipped() : RenderType.solid()), "Block model does not contain the current render type for the current graphics settings. Setting: " + Minecraft.useFancyGraphics() + " Expected: " + (Minecraft.useFancyGraphics() ? RenderType.cutout() : RenderType.solid()));
 
         helper.succeed();
     }
@@ -127,16 +166,29 @@ public class ModelRenderLayerTest extends BaseTestMod {
             return new BlockModelGenerators(blocks, items, models) {
                 @Override
                 public void run() {
+                    // CUTOUT BLOCK
                     var textures = PlantType.NOT_TINTED.getTextureMapping(Blocks.ACACIA_SAPLING);
                     var cutout = PlantType.NOT_TINTED.getCross().create(BLOCK.get(), textures, (name, model) -> {
                         var json = model.get().getAsJsonObject();
                         json.addProperty("render_type", "minecraft:cutout");
+                        json.addProperty("render_type_fast", "minecraft:solid");
                         this.modelOutput.accept(name, () -> json);
                     });
                     this.blockStateOutput.accept(createSimpleBlock(BLOCK.get(), cutout));
 
                     var item = ModelTemplates.FLAT_ITEM.create(ITEM.get(), TextureMapping.layer0(Blocks.ACACIA_SAPLING), this.modelOutput);
                     this.itemModelOutput.accept(ITEM.get(), ItemModelUtils.plainModel(item));
+
+
+                    // OLD LEAVES
+                    var oldLeavesCutout = ModelTemplates.LEAVES.create(OLD_LEAVES.get(), TextureMapping.cube(Blocks.OAK_LEAVES), (name, model) -> {
+                        var json = model.get().getAsJsonObject();
+                        json.addProperty("render_type", "minecraft:cutout_mipped");
+                        this.modelOutput.accept(name, () -> json);
+                    });
+                    this.blockStateOutput.accept(createSimpleBlock(OLD_LEAVES.get(), oldLeavesCutout));
+
+                    this.itemModelOutput.accept(OLD_LEAVES_ITEM.get(), ItemModelUtils.plainModel(oldLeavesCutout));
                 }
             };
         }
